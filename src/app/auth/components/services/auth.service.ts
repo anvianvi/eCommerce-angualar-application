a1e9481f7aa5e3a329264573d00cf955273a657e
Pavel <verbipo@gmail.com>
2024-05-21 21:15:56 +0200

    feat: draft of auth signal

---
+++ src/app/auth/components/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  isAuthenticated = signal(false);
+
+  constructor() {
+    const token = localStorage.getItem('my_auth_token');
+    this.isAuthenticated.set(!!token);
+  }
+
+  login(token: string): void {
+    localStorage.setItem('my_auth_token', token);
+    this.isAuthenticated.set(true);
+  }
+
+  logout() {
+    localStorage.removeItem('my_auth_token');
+    this.isAuthenticated.set(false);
+  }
+}
