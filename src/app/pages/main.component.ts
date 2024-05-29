f5ad74dd17a9af13ce7168fc47008bd867d5d861
Pavel <verbipo@gmail.com>
2024-05-29 19:39:33 +0200

    feat: clenup main, prevent fetching if products already exists

--- src/app/pages/main.component.ts
+++ src/app/pages/main.component.ts
@@ -22,31 +22,22 @@ import { ProductCardComponent } from '../components/product-card.componet';
   standalone: true,
   selector: 'app-main',
   template: `
-    <h1>This is main screen for logged-in users:</h1>
-    <p>Content will appear here later, but for now it's empty.</p>
-    <p>
-      If you need to get to the login or registration page, open the system
-      using the logout button under the profile-icon in the header.
-    </p>
-    @for (product of products(); track $index) {
-      <app-product-card [product]="product"></app-product-card>
-    }
+    <div class="products-list">
+      @for (product of products(); track $index) {
+        <app-product-card [product]="product"></app-product-card>
+      }
+    </div>
   `,
   styles: `
     ::ng-deep app-main {
-      margin: 45px auto 0 auto;
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      background: #f2f2f2;
-      // max-width: 330px;
-      padding: 15px;
     }
 
-    .buttons-container {
+    .products-list {
       display: flex;
-      gap: 20px;
+      justify-content: center;
       flex-wrap: wrap;
+      gap: 37px;
+      margin-bottom: 100px;
     }
   `,
 })
@@ -71,21 +62,23 @@ export class MainComponent implements OnInit {
       this.router.navigate(['/']);
     }
 
-    this.getProductsService.queryProducts().subscribe({
-      next: () => {
-        this.snackbarService.show(
-          'Customer token fetched successfully',
-          'Ok',
-          2000,
-        );
-      },
-      error: (err) => {
-        this.snackbarService.show(
-          `Failed to fetch Customer AccessToken, ${err}`,
-          'Ok',
-          2000,
-        );
-      },
-    });
+    if (this.products().length === 0) {
+      this.getProductsService.queryProducts().subscribe({
+        next: () => {
+          this.snackbarService.show(
+            'Customer token fetched successfully',
+            'Ok',
+            2000,
+          );
+        },
+        error: (err) => {
+          this.snackbarService.show(
+            `Failed to fetch Customer AccessToken, ${err}`,
+            'Ok',
+            2000,
+          );
+        },
+      });
+    }
   }
 }
