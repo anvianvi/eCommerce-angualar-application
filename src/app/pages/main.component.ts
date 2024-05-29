d4579175f1849bf111dbb6899bdb323402ffa668
Pavel <verbipo@gmail.com>
2024-05-29 11:48:18 +0200

    feat: draft of products modes

--- src/app/pages/main.component.ts
+++ src/app/pages/main.component.ts
@@ -7,9 +7,18 @@ import {
 } from '@angular/router';
 import { AuthenticationService } from '../core/services/authentication.service';
 import { MatButton } from '@angular/material/button';
+import { SnackbarService } from '../core/services/mat-snackbar.service';
+import { GetProductsService } from '../core/services/api/get-products.service';
+import { ProductCardComponent } from '../components/product-card.componet';
 
 @Component({
-  imports: [MatButton, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    MatButton,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    ProductCardComponent,
+  ],
   standalone: true,
   selector: 'app-main',
   template: `
@@ -19,6 +28,9 @@ import { MatButton } from '@angular/material/button';
       If you need to get to the login or registration page, open the system
       using the logout button under the profile-icon in the header.
     </p>
+    @for (product of products(); track $index) {
+      <app-product-card [product]="product"></app-product-card>
+    }
   `,
   styles: `
     ::ng-deep app-main {
@@ -27,7 +39,7 @@ import { MatButton } from '@angular/material/button';
       justify-content: center;
       flex-direction: column;
       background: #f2f2f2;
-      max-width: 330px;
+      // max-width: 330px;
       padding: 15px;
     }
 
@@ -43,14 +55,37 @@ export class MainComponent implements OnInit {
     return this.authenticationService.isAuthenticated();
   });
 
+  products = computed(() => {
+    return this.getProductsService.productsInStore();
+  });
+
   constructor(
     private authenticationService: AuthenticationService,
     private router: Router,
+    private getProductsService: GetProductsService,
+    private snackbarService: SnackbarService,
   ) {}
 
   ngOnInit(): void {
     if (!this.isAuthenticated()) {
       this.router.navigate(['/']);
     }
+
+    this.getProductsService.queryProducts().subscribe({
+      next: () => {
+        this.snackbarService.show(
+          'Customer token fetched successfully',
+          'Ok',
+          2000,
+        );
+      },
+      error: (err) => {
+        this.snackbarService.show(
+          `Failed to fetch Customer AccessToken, ${err}`,
+          'Ok',
+          2000,
+        );
+      },
+    });
   }
 }
