9d194d25c2bb30b9faaf9553885f935780219771
KamilMRogowski <kamil.rogowski94@gmail.com>
2024-05-29 15:26:35 +0200

    feat: seperate components into 3 files, style header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,42 @@
+import { Component, computed } from '@angular/core';
+import { ProfileStatusBarfoComponent } from '../profile-status-bar/profile-status-bar.component';
+import { AuthenticationService } from '../../core/services/authentication.service';
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive,
+  Router,
+} from '@angular/router';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  imports: [
+    MatButton,
+    ProfileStatusBarfoComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  isAuthenticated = computed(() => {
+    return this.authenticationService.isAuthenticated();
+  });
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+  ) {}
+
+  toLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  toRegistration(): void {
+    this.router.navigate(['/registration']);
+  }
+}
---
+++ src/app/components/profile-status-bar/profile-status-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../core/services/authentication.service';
+
+@Component({
+  imports: [MatButtonModule, MatMenuModule],
+  selector: 'app-profile-status-bar',
+  standalone: true,
+  templateUrl: './profile-status-bar.component.html',
+  styleUrl: './profile-status-bar.component.scss',
+})
+export class ProfileStatusBarfoComponent {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+  ) {}
+
+  openProfile(): void {
+    console.log('profile process');
+    // this.router.navigate(['/profile']);
+  }
+
+  logout(): void {
+    this.authenticationService.logout();
+  }
+}
